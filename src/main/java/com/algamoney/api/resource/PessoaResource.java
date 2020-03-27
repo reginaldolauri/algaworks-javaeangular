fa7436b61bf2189dc4a4b5300925f9b81bc137ae
reginaldolauri <reginaldo.lauri@gmail.com>
2020-03-27 18:10:45 -0300

    Implementação do recurso de inclusão de pessoas

--- src/main/java/com/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/algamoney/api/resource/PessoaResource.java
@@ -1,11 +1,19 @@
 package com.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algamoney.api.model.Pessoa;
 import com.algamoney.api.repository.PessoaRepository;
@@ -20,5 +28,14 @@ public class PessoaResource {
 	@GetMapping
 	public List<Pessoa> listar() {
 		return this.pessoaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = this.pessoaRepository.save(pessoa);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(pessoaSalva);
 	}
 }
