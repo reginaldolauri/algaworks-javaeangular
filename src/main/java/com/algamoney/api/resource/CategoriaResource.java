bb10c2663dd695826fc8df62e97de7635720f526
reginaldolauri <reginaldo.lauri@gmail.com>
2020-04-03 16:16:56 -0300

    ajustes no pom para add java assist, ajuste de driver do mysql e implementação do CORS

--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -29,7 +29,7 @@ public class CategoriaResource {
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
-	
+
 	@GetMapping
 	public List<Categoria> listar(){
 		return this.categoriaRepository.findAll();
